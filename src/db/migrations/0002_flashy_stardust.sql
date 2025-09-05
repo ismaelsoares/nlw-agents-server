700dc9bfb273022d1d721ef4a4b62de7409d778e
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2025-09-05 20:58:04 -0300

    feat: add create question, create room and get room questions

---
+++ src/db/migrations/0002_flashy_stardust.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "questions" RENAME COLUMN "create_at" TO "created_at";
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,6 +1,8 @@
 //Barrel File Pattern
+import { questions } from './questions.ts';
 import { rooms } from './rooms.ts';
 
 export const schema = {
   rooms,
+  questions,
 };
